bea485c94b57d58429a9e1bbec642971dda187dd
David Eichiner <david.eichiner@efs-techhub.com>
2026-09-01 10:28:22 +0200

    remove "bilder" folder

--- environments/nyx.nix
+++ environments/nyx.nix
@@ -1,6 +1,10 @@
 
 { config, pkgs, ... }:
 
+let
+  link = config.lib.file.mkOutOfStoreSymlink;   # Symlink auf Live-Pfad, nicht in den Store
+  sync = "${config.home.homeDirectory}/.sync";
+in
 {
 
   home.packages = with pkgs; [
@@ -8,6 +12,16 @@
     portfolio
     digikam
   ];
+
+  # Syncthing-Ordner als suffixlose Symlinks in ~ (Richtung steckt im Ordner-Präfix)
+  home.file = {
+    "dokumente".source   = link "${sync}/share-dokumente";
+    "notizen".source     = link "${sync}/share-notizen";
+    "Documents".source   = link "${sync}/backup-nyx/Documents";
+    "Downloads".source   = link "${sync}/backup-nyx/Downloads";
+    "Screenshots".source = link "${sync}/backup-nyx/Screenshots";
+  };
+
   # Override specific dotfiles
   # xdg.configFile."myapp/config".source = ./dotfiles/env1-config;
 }
